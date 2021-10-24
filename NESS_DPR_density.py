881b360bd7ef283ebbde5380beb773164cb35047
Sundar Srinivasan <s.srinivasan@irya.unam.mx>
2021-10-23 22:19:54 -0500

    Documentation and try/except for latex usage

--- NESS_DPR_density.py
+++ NESS_DPR_density.py
@@ -1,30 +1,69 @@
-def NESS_DPR_density(infile = 'NESS_Table1_scaled.vot'):
+from astropy.table import Table
+import numpy as np
+import matplotlib.pyplot as plt
+
+def texify(nums, decimals = 2):
+    #Account for any numbers that might not be in scientific notation
+    f = '%.' + str(decimals) + 'E'
+    from decimal import Decimal
+    numbers = np.array([f % Decimal(n) for n in nums])
+    #For each value in /numbers/, return a TeX string of the form $mantissa \times 10^{exponent}$
+    me = np.array([str(uu).split('E') if 'E' in str(uu) else [str(uu), '0'] for uu in numbers])
+    mantissa = [np.round(float(mant), decimals = decimals) for mant in me[:, 0]]
+    exponent = [int(exp) for exp in me[:, 1]]
+    return np.array(['$' + str(m) + '\\times 10^{' + str(e) + '}$' if e != 0 else str(m) for m, e in zip(mantissa, exponent)])
+
+def setPlotParams():
+    #the latex preamble setup here may result in errors for some.
+    plt.figure(figsize = (8, 8))
+    params = {'legend.fontsize': 'x-large',
+              'axes.labelsize':20,
+              'axes.titlesize':20,
+              'xtick.labelsize':20,
+              'ytick.labelsize':20}
+    try:
+        params['text.usetex'] = True
+        params['text.latex.preamble'] = r'\usepackage{bm}'
+        plt.rcParams.update(params)
+    except:
+        plr.rcParams.update(params)
+    return plt
+
+def NESS_DPR_density(infile = 'NESS_Table1_scaled_newer.vot'):
+    """
+        Reproduce Figure 9 and Table 4 from Scicluna et al. (2021)
+        Required input: VOTable containing data for Table 1 from the paper
+    """
     t = Table.read(infile, format = 'votable')
-    #t = t[t['Ext'] == 1].copy()
-    #
     tiers = ['very low', 'low', 'intermediate', 'high', 'extreme']
     ktiers = []
     for tier in tiers:
         ktiers.append(np.nonzero(t['sample'] == tier)[0])
+
+    """ Figure 9 from the paper """
     plt = setPlotParams()
     logdpr = [np.log10(t['GRAMS_DPR'][k]) for k in ktiers]
     _ = plt.boxplot(logdpr, meanline = True, showmeans = True, labels = tiers, whis = (2.5, 97.5))
-    plt.ylabel(r'$\log{({\rm DPR}\ [{\rm M}_\odot\ {\rm yr}^{-1}])}$')
+    try:
+        plt.ylabel(r'$\log{({\rm DPR}\ [{\rm M}_\odot\ {\rm yr}^{-1}])}$')
+    except:
+        plt.ylabel('log(DPR [Msun/yr])')
     plt.savefig('NESS_DPR_density.pdf', bbox_inches = 'tight')
     plt.close()
-    #total DPR for each bin
+    # total DPR for each bin
     DPR_total = np.array([t['GRAMS_DPR'][k].sum() for k in ktiers])
-    """Computing volumes for each distance bin"""
-    radius_old = np.array([0.0, 0.0, 0.400, 0.800, 2.0]) #old values used for original source list
-    radius = np.array([0.250, 0.300, 0.600, 1.200, 3.0]) #values used for extension list
-    scale_height = 0.1 #scale height in kpc for Tier 4 sources from Libby's plots.
+
+    """Table 4 from the paper """
     #Volumes for the higher tiers are affected by extinction in the Galactic Plane (|b| < 1.5 deg).
     #   For these tiers, volume_total = volume_inner + volume_outer, where
     #   volume_inner = (Tiers 2 and 3) = volume of sphere with inner radius
     #                = (Tier 4) = volume of cylinder with inner radius and height = scale height
-    #                               for Tier 4 from Libby's plots.
+    #                               for Tier 4 as estimated from, e.g., Figure 8 in the paper.
     #   volume_outer = volume of two cones of inner base radius `radius_old` and outer base radius `radius`
     #                   with vertex angles (90 - b) deg.
+    radius_old = np.array([0.0, 0.0, 0.400, 0.800, 2.0]) #old values used for original source list
+    radius = np.array([0.250, 0.300, 0.600, 1.200, 3.0]) #values used for extension list
+    scale_height = 0.1 #scale height in kpc for Tier 4 sources as estimated from, e.g., Figure 8 in the paper.
     volume_inner = 4/3 * np.pi * radius_old**3
     volume_inner[-1] = np.pi * radius_old[-1]**2 * scale_height
     b = 1.5 #Galactic latitude that is excluded due to the Plane
